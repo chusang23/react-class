09fd3486f118b788fb421d8bcd023a458e6aeaee
chusang23 <45816771+chusang23@users.noreply.github.com>
2021-07-20 15:08:40 +0900

    react

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  let posts = { color : 'yellow', fontSize: '30px' }
+  let [글제목, 글제목변경] = useState(['남자코트 추천','강남 우동 맛집', '파이썬 독학']);
+  let [따봉, 따봉변경] = useState(0);
+  let posts = '강남 고기 맛집';
+
   return (
     <div className="App">
       <div className="black-nav">
-        <div style={ posts }>개발 blog</div>
+        <div>개발 blog</div>
+      </div>
+      <button>버튼<span onclick={ () => { 글제목변경(['여자코트 추천','강남 우동 맛집', '파이썬 독학'])}}></span></button>
+      <div className="list">
+        <h3> { 글제목[0] } <span onClick={ () => { 따봉변경(따봉 + 1)}}>👍</span> {따봉} </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> { 글제목[1] } </h3>
+        <p>2월 18일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> { 글제목[2] } </h3>
+        <p>2월 19일 발행</p>
+        <hr/>
       </div>
     </div>
   );
