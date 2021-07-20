017f3b78fbee26b4b20368bfb8b4d8099785b81a
chusang23 <45816771+chusang23@users.noreply.github.com>
2021-07-20 18:29:58 +0900

    deep copy

--- blog/src/App.js
+++ blog/src/App.js
@@ -8,12 +8,18 @@ function App() {
   let [따봉, 따봉변경] = useState(0);
   let posts = '강남 고기 맛집';
 
+  function 제목바꾸기 (){
+    var newArray = [...글제목];
+    newArray[0] = '여자코트 추천';
+    글제목변경( newArray );
+  }
+
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 blog</div>
       </div>
-      <button>버튼<span onclick={ () => { 글제목변경(['여자코트 추천','강남 우동 맛집', '파이썬 독학'])}}></span></button>
+      <button onClick={ 제목바꾸기 }>버튼</button>
       <div className="list">
         <h3> { 글제목[0] } <span onClick={ () => { 따봉변경(따봉 + 1)}}>👍</span> {따봉} </h3>
         <p>2월 17일 발행</p>
