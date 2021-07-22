ffed91f3e6deaed8f44dc8a45f2c132f90f02b6d
chusang23 <45816771+chusang23@users.noreply.github.com>
2021-07-22 16:12:19 +0900

    Update App.js

--- blog/src/App.js
+++ blog/src/App.js
@@ -6,8 +6,8 @@ function App() {
 
   let [글제목, 글제목변경] = useState(['남자코트 추천','강남 우동 맛집', '파이썬 독학']);
   let [따봉, 따봉변경] = useState(0);
-
   let [modal,modal변경] = useState(false);
+  let [누른제목,누른제목변경] = useState(0);
 
   let posts = '강남 고기 맛집';
 
@@ -39,11 +39,15 @@ function App() {
         <hr/>
       </div>
 
+      <button onClick= {() => {누른제목변경(0)}}>버튼1</button>
+      <button onClick= {() => {누른제목변경(1)}}>버튼2</button>
+      <button onClick= {() => {누른제목변경(2)}}>버튼3</button>
+
       <button onClick={ () => { modal변경(!modal) } } >버튼</button>
 
       {
         modal === true
-        ? <Modal 글제목 = {글제목}></Modal>
+        ? <Modal 글제목 = {글제목} 누른제목 = {누른제목}></Modal>
         : null
       }
 
@@ -54,7 +58,7 @@ function App() {
 function Modal(props){
   return (
     <div className="modal">
-    <h2>{props.글제목[0]}</h2>
+    <h2>{props.글제목[props.누른제목]}</h2>
     <p>날씨</p>
     <p>상세내용</p>
   </div>
