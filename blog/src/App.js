47836b114d8196e5aca5c833a4952423bd347425
chusang23 <45816771+chusang23@users.noreply.github.com>
2021-07-23 13:47:45 +0900

    props

--- blog/src/App.js
+++ blog/src/App.js
@@ -6,6 +6,7 @@ function App() {
 
   let [글제목, 글제목변경] = useState(['남자코트 추천','강남 우동 맛집', '파이썬 독학']);
   let [따봉, 따봉변경] = useState(0);
+
   let [modal,modal변경] = useState(false);
   let [누른제목,누른제목변경] = useState(0);
 
@@ -22,22 +23,18 @@ function App() {
       <div className="black-nav">
         <div>개발 blog</div>
       </div>
-      <button onClick={ 제목바꾸기 }>버튼</button>
+
+    {
+      글제목.map(function(글 , i){
+      return (
       <div className="list">
-        <h3> { 글제목[0] } <span onClick={ () => { 따봉변경(따봉 + 1)}}>👍</span> {따봉} </h3>
+        <h3 onClick= {() => {누른제목변경(i)} }> { 글 } <span onClick={ () => { 따봉변경(따봉 + 1)}}>👍</span> {따봉} </h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
-      <div className="list">
-        <h3> { 글제목[1] } </h3>
-        <p>2월 18일 발행</p>
-        <hr/>
-      </div>
-      <div className="list">
-        <h3> { 글제목[2] } </h3>
-        <p>2월 19일 발행</p>
-        <hr/>
-      </div>
+      )
+    })
+  }
 
       <button onClick= {() => {누른제목변경(0)}}>버튼1</button>
       <button onClick= {() => {누른제목변경(1)}}>버튼2</button>
@@ -66,3 +63,4 @@ function Modal(props){
 }
 
 export default App;
+
