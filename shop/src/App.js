cbc53de65ce5c550a0ffa0781d90f4fd8177a8cc
chusang23 <45816771+chusang23@users.noreply.github.com>
2021-07-30 17:08:29 +0900

    react bootstrap

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
-<button type="button" class="btn btn-primary">Primary</button>
 
 function App() {
   return (
     <div className="App">
-
+      <button type="button" class="btn btn-primary">Primary</button>
     </div>
   );
 }
