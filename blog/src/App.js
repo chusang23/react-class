8aa911f866d26b2473e1a731df541efb536ff881
chusang23 <45816771+chusang23@users.noreply.github.com>
2021-12-28 23:07:41 +0900

    react

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,12 +1,18 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let posts = '강남 고기 맛집';
+
   return (
     <div className="App">
       <div className="black-nav">
-        <div>개발 Blog</div>
+        <div style={ {color : 'blue', fontSize : '30px' } }>개발 Blog</div>
       </div>
+      <img src={ logo } />
+      <h4>{ posts }</h4>
     </div>
   );
 }
