2be5dfb8eaa3f77b3012de2511ad46c7dfb4ebcf
chusang23 <45816771+chusang23@users.noreply.github.com>
2022-02-03 18:10:35 +0900

    react

--- blog/src/App.js
+++ blog/src/App.js
@@ -24,23 +24,12 @@ function App() {
         <div>개발 Blog</div>
       </div>
       <button onClick={ 제목바꾸기 }>버튼</button>
-    <div className="list">
-      <h3>{ 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉 + 1) }}>👍</span> {따봉} </h3>
-      <p>2월 17일 발행</p>
-      <hr/>
-      <h3>{ 글제목[1] }</h3>
-      <p>2월 18일 발행</p>
-      <hr/>
-      <h3>{ 글제목[2] }</h3>
-      <p>2월 19일 발행</p>
-      <hr/>
-    </div>
 
     {
-      글제목.map(function(a){
+      글제목.map(function(a, i){
         return (
         <div className="list">
-          <h3>{ a }</h3>
+          <h3 onClick={ ()=>{누른제목변경(i)} }> { a } <span onClick={ ()=>{ 따봉변경(따봉 + 1) }}>👍</span> {따봉} </h3>
           <p>2월 18일 발행</p>
           <hr/>
         </div>
@@ -49,21 +38,21 @@ function App() {
     }
 
 
-    <button onClick={ ()=>{} }>버튼1</button>
-    <button onClick={ ()=>{} }>버튼2</button>
-    <button onClick={ ()=>{} }>버튼3</button>
+    {/* <button onClick={ ()=>{누른제목변경(0)} }>버튼1</button>
+    <button onClick={ ()=>{누른제목변경(1)} }>버튼2</button>
+    <button onClick={ ()=>{누른제목변경(2)} }>버튼3</button> */}
 
     <button onClick={ ()=>{modal변경(!modal)} }>열고닫기</button>
     {
     modal === true
-    ? <Modal></Modal>
+    ? <Modal 글제목={글제목} 누른제목={누른제목}></Modal>
     : null
 }
 </div>
   );
 }
 
-function Modal(){
+function Modal(props){
   return (
     <div className="modal">
     <h2> {props.글제목[props.누른제목]}</h2>
